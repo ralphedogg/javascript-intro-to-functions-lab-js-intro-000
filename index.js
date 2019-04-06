ae196a741c8ebb1654bcc83b4a89c6ef9af2728a
Ralph Nicholson <nicholson.re@gmail.com>
2019-04-06 18:11:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = string.toLowerCase
-  string = lowercase
-  return "I can\'t hear you";
+  if (string === string.toLowerCase()) {
+    return "I can\'t hear you";
+  }
 }
